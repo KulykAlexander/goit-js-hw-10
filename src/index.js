b116b5c6b486ccafb5350dc8c34ff2dbdff6d155
KulykAlexander <kulikau@gmail.com>
2022-12-13 11:01:04 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import Notiflix from 'notiflix';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countrysList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
