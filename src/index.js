a5fef4f6f3ba28217580e92e47bbfa5f090e1908
KulykAlexander <kulikau@gmail.com>
2022-12-20 19:30:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,3 +8,5 @@ const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 const countrysList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
+searchBoxEll.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
