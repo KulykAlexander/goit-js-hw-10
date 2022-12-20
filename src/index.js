c07c0efdc7f78032cc8960a8d7500d6949e7da16
KulykAlexander <kulikau@gmail.com>
2022-12-20 19:27:36 +0200

    debounce install

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import Notiflix from 'notiflix';
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
