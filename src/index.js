b08c794f98c6093fc9e6b1919c42231eb1ebf565
KulykAlexander <kulikau@gmail.com>
2022-12-22 20:24:56 +0200

    task

--- src/index.js
+++ src/index.js
@@ -6,7 +6,64 @@ import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('#search-box');
-const countrysList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
 
-searchBoxEll.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+inputEl.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+  let name = e.target.value.trim();
+  if (name === '') {
+    clearContent();
+  } else {
+    fetchHendler(name);
+  }
+}
+function fetchHendler(name) {
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        clearContent();
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (data.length >= 2 && data.length <= 10) {
+        clearContent();
+        countryListMarkUp(data);
+        return;
+      }
+      if ((data.length = 1)) {
+        clearContent();
+        countryListMarkUp(data);
+        countryInfoMarkUp(data);
+        return;
+      }
+    })
+    .catch(error => {
+      clearContent();
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function countryInfoMarkUp(data) {
+  const markUp = data.map(({ capital, population, languages }) => {
+    return `<p>Capital: ${capital}</p><p>Population: ${population}</p><p>Languages: ${Object.values(
+      languages
+    )}</p>`;
+  });
+  countryInfoEl.innerHTML = markUp.join('');
+}
+
+function countryListMarkUp(data) {
+  const markUp = data.map(({ name: { official }, flags: { svg } }) => {
+    return `<li><img src=${svg} width='50'></img>${official}</li>`;
+  });
+  countryListEl.innerHTML = markUp.join('');
+}
+
+function clearContent() {
+  countryListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
+}
